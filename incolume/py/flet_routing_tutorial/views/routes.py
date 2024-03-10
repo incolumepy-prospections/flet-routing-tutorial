63979107a86ce8e87a3b04e60ea22f467d431850
Britodfbr <britodfbr@gmail.com>
2024-03-10 00:52:49 -0300

    refactor: Aplicado PEP8

--- incolume/py/flet_routing_tutorial/views/routes.py
+++ incolume/py/flet_routing_tutorial/views/routes.py
@@ -1,14 +1,19 @@
-from incolume.py.flet_routing_tutorial.views.Router import Router, DataStrategyEnum
-from incolume.py.flet_routing_tutorial.views.index_view import IndexView
-from incolume.py.flet_routing_tutorial.views.profile_view import ProfileView
-from incolume.py.flet_routing_tutorial.views.settings_view import SettingsView
-from incolume.py.flet_routing_tutorial.views.data_view import DataView
+"""routes module."""
+
+from incolume.py.flet_routing_tutorial.views.Router import (
+    Router,
+    DataStrategyEnum,
+)
+from incolume.py.flet_routing_tutorial.views.index_view import index_view
+from incolume.py.flet_routing_tutorial.views.profile_view import profile_view
+from incolume.py.flet_routing_tutorial.views.settings_view import settings_view
+from incolume.py.flet_routing_tutorial.views.data_view import data_view
 
 router = Router(DataStrategyEnum.QUERY)
 
 router.routes = {
-    "/": IndexView,
-    "/profile": ProfileView,
-    "/settings": SettingsView,
-    "/data": DataView,
+    '/': index_view,
+    '/profile': profile_view,
+    '/settings': settings_view,
+    '/data': data_view,
 }
